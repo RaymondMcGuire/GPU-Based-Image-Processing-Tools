21579ad21cac3eb6350eb859a19e7bf7074445c0
オウ　キョク <m02171017e@edu.teu.ac.jp>
2018-08-26 17:24:09 +0900

    normal map tools completed
    using extra lib from my other library
    created the normap map generate tools

---
+++ tools_webgl/ts_scripts/build/readme-mac.md
@@ -0,0 +1,3 @@
+#compile the scripts for mac
+cd ..
+tsc --out ../../scripts/main.js @compile-list.txt
--- tools_webgl/ts_scripts/lib/image_utils.ts
+++ tools_webgl/ts_scripts/lib/image_utils.ts
@@ -4,12 +4,14 @@
  *  simple func for processing image
  * 
  * ========================================================================= */
-module EcognitaMathLib {
+/// <reference path="../extlib/vector.ts" />
+ module EcognitaMathLib {
     export class ImageView{
         canvas:any;
         context:any;
         image:any;
-        imageDataBuffer:any;
+        imageDataBuffer:Array<number>[][];
+        grayDataBuffer:number[][];
         constructor(cvs:any,url:string){
             var image = new Image();
             image.src = url;
@@ -17,30 +19,38 @@ module EcognitaMathLib {
             this.canvas = cvs;
             this.context =  cvs.getContext('2d');
             this.image = image;
-            this.imageDataBuffer = [];
+            this.imageDataBuffer = new Array(cvs.height);
+            this.grayDataBuffer = new Array(cvs.height);
         }
 
         private convertGray(r:number, g:number, b:number) {
             return 0.299 * r + 0.578 * g + 0.114 * b;
         }
 
-        drawImage(left:number = 0, top:number = 0){
-            this.image.onload =  (() => { 
+        //transfrom -1~1 to 0~255
+        private convEdgeVal2RGB(val:number){
+            return (val + 1.0) * (255.0 / 2.0);
+        }
+
+        readImageData(left:number = 0, top:number = 0){ 
                 this.context.drawImage(this.image, left, top);
                 var cvs = this.canvas;
                 let imgData = this.getImageData();
                 for (var j = 0; j < cvs.height; j++) {
+                    this.imageDataBuffer[j]=new Array(this.canvas.width);
+                    this.grayDataBuffer[j] =new Array(this.canvas.width);
                     for (var i = 0; i < cvs.width; i++) {
                         var index = (j * cvs.width + i) * 4;
-                        this.imageDataBuffer.push(imgData.data[index + 0]);
-                        this.imageDataBuffer.push(imgData.data[index + 1]);
-                        this.imageDataBuffer.push(imgData.data[index + 2]);
-                        this.imageDataBuffer.push(imgData.data[index + 3]);
+                        this.imageDataBuffer[j][i] = new Array(4);
+                        this.imageDataBuffer[j][i][0] =imgData.data[index + 0];
+                        this.imageDataBuffer[j][i][1] =imgData.data[index + 1];
+                        this.imageDataBuffer[j][i][2] =imgData.data[index + 2];
+                        this.imageDataBuffer[j][i][3] =imgData.data[index + 3];
+
+                        var grayVal = this.convertGray(this.imageDataBuffer[j][i][0],this.imageDataBuffer[j][i][1],this.imageDataBuffer[j][i][2]);
+                        this.grayDataBuffer[j][i] = grayVal;
                     }
-                
                 }
-                this.drawGrayImage();
-            });
         }
 
         drawGrayImage(){
@@ -51,7 +61,8 @@ module EcognitaMathLib {
                 for (var j = 0; j < cvs.height; j++) {
                     for (var i = 0; i < cvs.width; i++) {
                         var index = (j * cvs.width + i) * 4;
-                        var grayVal = this.convertGray(this.imageDataBuffer[index + 0],this.imageDataBuffer[index + 1],this.imageDataBuffer[index + 2]);
+                        var grayVal = this.grayDataBuffer[j][i];
+                    
                         imgData.data[index + 0] = grayVal;
                         imgData.data[index + 1] = grayVal;
                         imgData.data[index + 2] = grayVal;
@@ -62,13 +73,64 @@ module EcognitaMathLib {
             
         }
 
-        drawNormalMap(factor:number=1.0,attenuation:number=1.56){
-            this.image.onload =  (() => { 
-                this.context.drawImage(this.image, 0, 0);
-               
+        private sobel_filter(data_buffer:any,idx_y:number,idx_x:number,type:string="vertical",kernel_size:number=3){
+            // 1  2  1   vertical     1  0  -1  horizontal
+            // 0  0  0                2  0  -2
+            //-1 -2 -1                1  0  -1
             
+            var vertical_sobel =[[-1,-2,-1],
+                                 [ 0, 0, 0],
+                                 [ 1, 2, 1]];
+            var horizontal_sobel =[[-1, 0, 1],
+                                   [-2, 0, 2],
+                                   [-1, 0, 1]];
+            var Val = 0;
+            var center = Math.floor(kernel_size/2);
+            for(var i=-center;i<=center;i++){
+                for(var j =-center;j<=center;j++){
 
-             });
+                    //chk out of range
+                    if(idx_y+i<0 || idx_x+j<0 || idx_y+i>=this.canvas.height || idx_x+j>=this.canvas.width)continue;
+
+                    if(type == "vertical"){
+                        Val+=data_buffer[idx_y+i][idx_x+j]*vertical_sobel[center+i][center+j];
+                       
+                    }else if(type == "horizontal"){
+                        Val+=data_buffer[idx_y+i][idx_x+j]*horizontal_sobel[center+i][center+j];
+                    }
+                }
+            }
+            return Val;
+        }
+
+        //algo referenced by 
+        //https://stackoverflow.com/questions/2368728/can-normal-maps-be-generated-from-a-texture/2368794#2368794
+        //https://github.com/cpetry/NormalMap-Online/blob/gh-pages/javascripts/normalMap.js
+        drawNormalMap(strength:number=2.5,level:number=7.0){
+
+            this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            var cvs = this.canvas;
+            let imgData = this.getImageData();
+            for (var j = 0; j < cvs.height; j++) {
+                for (var i = 0; i < cvs.width; i++) {
+                    var index = (j * cvs.width + i) * 4;
+
+                    var Dx = this.sobel_filter(this.grayDataBuffer,j,i,"horizontal");
+                    var Dy = this.sobel_filter(this.grayDataBuffer,j,i,"vertical");
+                    var Dz = 1.0/strength * (1.0 + Math.pow(2.0, level));
+
+                    //normalize
+                    var v = new EcognitaMathLib.Vector(3,[Dx,Dy,Dz]);
+                    v.normalize();
+                    var data = v.data();
+             
+                    imgData.data[index + 0] = this.convEdgeVal2RGB(data[0]);
+                    imgData.data[index + 1] = this.convEdgeVal2RGB(data[1]);;
+                    imgData.data[index + 2] = this.convEdgeVal2RGB(data[2]);;
+                    imgData.data[index + 3] = 255;
+                }
+            }
+            this.context.putImageData(imgData, 0, 0);
         }
 
         getImageData(){
