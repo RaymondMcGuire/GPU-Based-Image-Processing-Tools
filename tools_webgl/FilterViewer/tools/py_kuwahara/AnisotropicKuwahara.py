bbfd9682e2cd6cf3b189d2921f7536d11311b994
RaymondMcGuire <raymondmg@foxmail.com>
2018-12-03 13:35:24 +0900

    fixed Anisotropic visual script

--- tools_webgl/FilterViewer/tools/py_kuwahara/AnisotropicKuwahara.py
+++ tools_webgl/FilterViewer/tools/py_kuwahara/AnisotropicKuwahara.py
@@ -6,7 +6,6 @@ Created on Sun Oct  7 18:39:00 2018
 """
 import numpy as np
 import SST
-import Gaussian as ga
 import cv2 as cv
 
 class AnisotropicKuwahara:
@@ -43,7 +42,6 @@ class AnisotropicKuwahara:
                 else:
                     A[j,i] = (lambda1 - lambda2)/(lambda1 + lambda2)
                 
-                A[j,i] = 1
                 #visualization Anisotropic
                 anisotropic_image[j,i,0] = visual_image[0,int(255*A[j,i]),0]
                 anisotropic_image[j,i,1] = visual_image[0,int(255*A[j,i]),1]
@@ -54,141 +52,13 @@ class AnisotropicKuwahara:
         self.A = A
         self.PHI = PHI
         cv.imwrite("./img/anisotropic_image.png",anisotropic_image)    
+        return anisotropic_image
         
-    
-    def calc(self):
-        anisotropic_kuwahara_image = np.zeros((self.height,self.width,self.channel))
-        cnt = 0
-        cnt_list = []
-        EPS = 1e-6
-
-        for j in range(self.height):
-            for i in range(self.width):
-                cnt += 1
-            
-                num_count = round(100*cnt/(self.height*self.width),0)
-                
-                if num_count not in cnt_list:
-                    cnt_list.append(num_count)
-                    print("current progress:"+str(num_count)+"%")
-                
-                v_sum = np.zeros((self.div_num,3))
-                v_var = np.zeros((self.div_num,3))
-                v_weight = np.zeros(self.div_num)
-                
-                aniso = self.A[j,i]
-                theta = -self.PHI[j,i]
-                sx = self.alpha / (aniso + self.alpha)
-                sy = (self.alpha + aniso) / self.alpha
-                
-                for dy in range(-self.kernel_size,self.kernel_size+1):
-                    for dx in range(-self.kernel_size,self.kernel_size+1):
-                        if dx==0 and dy == 0:
-                            continue
-                        dx2 = int(sx * (np.cos(theta) * dx - np.sin(theta) * dy))
-                        dy2 = int(sy * (np.sin(theta) * dx + np.cos(theta) * dy))
-                        xx = i+dx2
-                        yy = j+dy2
-                        if xx>=0 and yy>=0 and xx<self.width and yy<self.height:
-                            theta = np.arctan2(dy2, dx2) + np.pi
-                            k = int(np.floor(theta / self.angle)) % self.div_num
-                            d2 = dx2 * dx2 + dy2 * dy2
-                            g = np.exp(-d2 / (2.0 * self.kernel_size))
-                            oB = self.image[yy,xx,0]
-                            oG = self.image[yy,xx,1]
-                            oR = self.image[yy,xx,2]
-                            v_sum[k,0] += g * oB
-                            v_sum[k,1] += g * oG
-                            v_sum[k,2] += g * oR
-                            v_var[k,0] += g * oB * oB
-                            v_var[k,1] += g * oG * oG
-                            v_var[k,2] += g * oR * oR
-                            v_weight[k] += g
-                                    
-                                    
-                de = np.zeros(3)
-                nu = np.zeros(3)
-                for d in range(self.div_num):
-                    if v_weight[d]!=0:
-                        v_sum[d,0] = v_sum[d,0]/v_weight[d]
-                        v_sum[d,1] = v_sum[d,1]/v_weight[d]
-                        v_sum[d,2] = v_sum[d,2]/v_weight[d]
-                        
-                        v_var[d,0] = v_var[d,0]/v_weight[d]
-                        v_var[d,1] = v_var[d,1]/v_weight[d]
-                        v_var[d,2] = v_var[d,2]/v_weight[d]
-                    else:
-                        v_sum[d,0] = 0
-                        v_var[d,0] = 0       
-                        
-                        v_sum[d,1] = 0
-                        v_var[d,1] = 0    
-                        
-                        v_sum[d,2] = 0
-                        v_var[d,2] = 0    
-
-                    v_var[d,0] = np.abs(v_var[d,0] - v_sum[d,0] * v_sum[d,0]) 
-                    v_var[d,1] = np.abs(v_var[d,1] - v_sum[d,1] * v_sum[d,1]) 
-                    v_var[d,2] = np.abs(v_var[d,2] - v_sum[d,2] * v_sum[d,2])                     
-                    
-                    if v_var[d,0] > EPS:
-                        v_var[d,0] = np.sqrt(v_var[d,0])
-                    else:
-                        v_var[d,0] = EPS
-                        
-                    if v_var[d,1] > EPS:
-                        v_var[d,1] = np.sqrt(v_var[d,1])
-                    else:
-                        v_var[d,1] = EPS
-                    
-                    if v_var[d,2] > EPS:
-                        v_var[d,2] = np.sqrt(v_var[d,2])
-                    else:
-                        v_var[d,2] = EPS
-                        
-                    w0 = 1/(1 + np.power(v_var[d,0],self.q))
-                    w1 = 1/(1 + np.power(v_var[d,1],self.q))
-                    w2 = 1/(1 + np.power(v_var[d,2],self.q))
-                    
-                    de[0] += w0 * v_sum[d,0]
-                    de[1] += w1 * v_sum[d,1]
-                    de[2] += w2 * v_sum[d,2]
-                    
-                    nu[0] += w0
-                    nu[1] += w1
-                    nu[2] += w2
-                    
-                    
-                if nu[0] > EPS:
-                    valB = de[0]/nu[0]
-                else:
-                    valB = self.image[j,i,0]
-                
-                if nu[1] > EPS:
-                    valG = de[1]/nu[1]
-                else:
-                    valG = self.image[j,i,1]
-                    
-                if nu[2] > EPS:
-                    valR = de[2]/nu[2]
-                else:
-                    valR = self.image[j,i,2]
-                
-                anisotropic_kuwahara_image[j,i,0] = int(valB)
-                anisotropic_kuwahara_image[j,i,1] = int(valG)
-                anisotropic_kuwahara_image[j,i,2] = int(valR)
-        self.res_image = anisotropic_kuwahara_image
-        return anisotropic_kuwahara_image
-
-
-#img = cv.imread('./img/anim.png')
-#sst_func = SST.SST(img,SST.SST_TYPE.CLASSIC)
-#sst_image = sst_func.cal(9)
-
-gaussian_func = ga.Gaussian()
-gfilter = gaussian_func.sector_calc(32)
-cv.imwrite("./img/k0.png",gfilter)         
-#aniso_kuwahara_func = AnisotropicKuwahara(img,sst_image)
-#aniso_kuwahara_image = aniso_kuwahara_func.calc()
-#cv.imwrite("./img/aniso_kuwahara_image.png",aniso_kuwahara_image)         
+img = cv.imread('./img/anim.png')
+sst_func = SST.SST(img,SST.SST_TYPE.CLASSIC)
+sst_image = sst_func.cal(5)
+       
+aniso_kuwahara_func = AnisotropicKuwahara(img,sst_image)
+anisotropic_image = aniso_kuwahara_func.param_anisotropy()
+  
 
--- tools_webgl/FilterViewer/tools/py_kuwahara/SST.py
+++ tools_webgl/FilterViewer/tools/py_kuwahara/SST.py
@@ -9,8 +9,7 @@ Created on Wed Oct  3 10:51:24 2018
 import cv2 as cv
 import numpy as np
 from enum import Enum
-
-import Gaussian
+import Gaussian 
 
 class SST_TYPE(Enum):
     CLASSIC = 1
@@ -25,11 +24,7 @@ class SST:
         self.sigma = sigma
         height,width,channel = img.shape
         dimage = np.float32(img)
-        #convert to 1/255
-        for j in range(height):
-            for i in range(width):
-                for c in range(channel):
-                    dimage[j,i,c] = dimage[j,i,c]/255.0
+
                 
         
         gray = cv.cvtColor(dimage, cv.COLOR_BGR2GRAY)
@@ -45,36 +40,6 @@ class SST:
                 fy = sobely[j,i]
                 tensor_image[j,i] = (fx*fx,fy*fy,fx*fy)
         
-        """
-        structure_tensor_image = np.zeros((height,width,3))
-        for t in range(iter):
-            for j in range(height):
-                for i in range(width):
-                    E = tensor_image[j,i,0]
-                    F = tensor_image[j,i,1]
-                    G = tensor_image[j,i,2]
-                    eng = np.sqrt(E*E+2*F*F+G*G)
-                    if eng>tau:
-                        sumE = 0
-                        sumF = 0
-                        sumG = 0
-                        cnt = 0
-                        for idx in range(len(box_filter)):
-                            xx = box_filter[idx][0] + i
-                            yy = box_filter[idx][1] + j
-                            if yy <height  and yy>=0 and xx<width and xx>=0:
-                                sumE += tensor_image[yy,xx,0]
-                                sumF += tensor_image[yy,xx,1]
-                                sumG += tensor_image[yy,xx,2]
-                                cnt+=1
-                        structure_tensor_image[j,i,0]= sumE/cnt
-                        structure_tensor_image[j,i,1]= sumF/cnt
-                        structure_tensor_image[j,i,2]= sumG/cnt
-                    else:
-                        structure_tensor_image[j,i] = tensor_image[j,i]
-            tensor_image =  structure_tensor_image
-        cv.imwrite("./img/structure_tensor_image.png",structure_tensor_image)
-        """     
         #sigma = 2 * self.sigma * self.sigma     
         #smooth_structure_tensor = cv.GaussianBlur(tensor_image,(kernel_size,kernel_size),sigma)
 
@@ -82,6 +47,7 @@ class SST:
         smooth_structure_tensor = gaussian_func.calc(tensor_image,2,height,width,channel)
         print("generated smooth structure tensor!")
         self.smooth_structure_tensor = smooth_structure_tensor
+        cv.imwrite("./img/smooth_structure_tensor.png",smooth_structure_tensor)
         return smooth_structure_tensor
     
     def cal(self,kernel_size=5):
